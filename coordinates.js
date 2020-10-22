cb87c3810d6f733d4b9223571c2d23a45a45b2b1
Therealajahn <therealajahn@gmail.com>
2020-10-22 08:48:57 -0500

    finished grid algorithm

--- coordinates.js
+++ coordinates.js
@@ -1,17 +1,20 @@
-generateGridAreas("test");
-
-function generateGridAreas(el){
+generateGridAreas("test",10);
+//use even numbers for length
+function generateGridAreas(el,length){
     let grid = document.getElementsByClassName(el)[0];
     let coordinates = '';
     
     let c = 0;
-    while(c < 100){
-        if(c % 10 === 0){
+    let y = length / 2;
+    while(c < length * length){
+        let x = c % 10 - (length / 2);
+        if(c % length === 0){
                coordinates += '"';
         }
-         coordinates += `${x}`;
-        if(c % 10 === 9){
+         coordinates += `x${x}y${y} `;
+        if(c % length === 9){
             coordinates += '"\n';
+            y--;
         }
         
         
